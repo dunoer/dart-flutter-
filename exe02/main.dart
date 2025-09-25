260e13b9d5ae57a7daf257fba69793aaf904c8f7
dunoer <edjrdu12@gmail.com>
2025-09-25 10:58:43 -0300

    update ex02

--- exe02/main.dart
+++ exe02/main.dart
@@ -1,10 +1,13 @@
+import 'dart:io';
+
 void main() {
-  Conta maria = new Conta();
-  maria.depositar(200);
-  maria.saque(100);
-  maria.nome = 'Maria';
-  maria.tipoConta = 'Poupança';
-  maria.meusdados();
+  Conta c1 = new Conta();
+  c1.abrirConta();
+  c1.tipoConta = 'cc';
+  c1.nome = 'Duno';
+  c1.depositar();
+  c1.saque();
+  c1.meusdados();
 }
 
 class Conta {
@@ -12,6 +15,19 @@ class Conta {
   String nome = '';
   String tipoConta = '';
   int saldo = 0;
+  bool status = false;
+
+  Conta() {
+    this.status = false;
+    this.saldo = 0;
+  }
+  bool getStatus() {
+    return status;
+  }
+
+  setStatus(s) {
+    status = s;
+  }
 
   int getNumConta() {
     return numConta;
@@ -45,12 +61,70 @@ class Conta {
     saldo = s;
   }
 
-  void depositar(int di) {
-    this.saldo = saldo + di;
+  void depositar() {
+    if (this.status) {
+      stdout.write('Qual valor deseja depositar?  ');
+      String? dg1 = stdin.readLineSync();
+      int? valor = int.tryParse(dg1 ?? "");
+      if (valor == null || valor <= 0) {
+        print('Valor de deposivo invalido');
+        return;
+      }
+      this.saldo += valor;
+    } else {
+      print('Sua Conta esta fechada');
+    }
   }
 
-  void saque(int sa) {
-    this.saldo = saldo - sa;
+  void saque() {
+    if (this.status && this.saldo > 0) {
+      stdout.write(
+        'Qual valor deseja sacar?  --Saldo disponivel: ${this.saldo} ',
+      );
+
+      String? dg2 = stdin.readLineSync();
+      int? saque = int.tryParse(dg2 ?? "");
+      if (saque == null || saque <= 0) {
+        print('Saque invalido');
+        return;
+      }
+      if (saque <= this.saldo) {
+        this.saldo -= saque;
+      } else {
+        print(
+          'Você não tem dinheiro suficiente para realizar esse saque  --Saldo disponivel: ${this.saldo}',
+        );
+      }
+    } else {
+      print('Sua conta esta fechada');
+    }
+  }
+
+  void abrirConta() {
+    this.status = true;
+    if (this.tipoConta == 'cc') {
+      this.saldo += 50;
+    } else {
+      this.saldo += 150;
+    }
+  }
+
+  void fecharConta() {
+    if (this.saldo == 0) {
+      this.status = false;
+    } else if (this.saldo > 0) {
+      print('Você deve fazer o saque do saldo na conta ${this.saldo}');
+    } else {
+      print('Você nao pode fechar a conta pois tem saldo negativo');
+    }
+  }
+
+  void pagarMensal() {
+    if (this.tipoConta == 'cc') {
+      this.saldo -= 12;
+    } else {
+      this.saldo -= 20;
+    }
   }
 
   void meusdados() {
