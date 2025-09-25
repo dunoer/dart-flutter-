55b20fcd3b8e9b39353d46acfe68abf35b4ed0b5
dunoer <edjrdu12@gmail.com>
2025-09-25 15:20:57 -0300

    up

--- exe02/main.dart
+++ exe02/main.dart
@@ -1,13 +1,25 @@
 import 'dart:io';
 
 void main() {
-  Conta c1 = new Conta();
-  c1.abrirConta();
-  c1.tipoConta = 'cc';
-  c1.nome = 'Duno';
-  c1.depositar();
-  c1.saque();
-  c1.meusdados();
+  int? nume;
+  do {
+    stdout.write(
+      '--- Digite o que deseja --- \n1-Abrir nova conta \n0- Sair \n',
+    );
+    String? entrada = stdin.readLineSync();
+    nume = int.tryParse(entrada ?? "");
+    if (nume == null || (nume != 0 && nume != 1)) {
+      print(' \n\nERRO: Por favor digite numero 1 ou 0\n\n');
+      continue;
+    }
+  } while (nume != 1 && nume != 0);
+
+  if (nume == 1) {
+    Conta c1 = new Conta();
+    c1.abrirConta();
+  } else {
+    print('Fim do Programa');
+  }
 }
 
 class Conta {
@@ -41,8 +53,8 @@ class Conta {
     return nome;
   }
 
-  setNome(no) {
-    nome = no;
+  setNome(entrada) {
+    nome = entrada;
   }
 
   String getTipoConta() {
@@ -101,13 +113,51 @@ class Conta {
   }
 
   void abrirConta() {
-    this.status = true;
-    if (this.tipoConta == 'cc') {
-      this.saldo += 50;
+    setStatus(true);
+    stdout.write('--> Digite seu nome: ');
+    String? entrada = stdin.readLineSync();
+    setNome(entrada);
+
+    String? entrada2;
+    do {
+      stdout.write(
+        '--> Digite tipo de conta cc ou cp: Conta corrente ou conta poupança ',
+      );
+      entrada2 = stdin.readLineSync();
+      if (entrada2 == '' || (entrada2 != 'cp' && entrada2 != 'cc')) {
+        print('\n\nERRO: Por favor digite cc ou cp');
+        continue;
+      }
+    } while (entrada2 != 'cc' && entrada2 != 'cp');
+    setTipoConta(entrada2);
+    if (tipoConta == 'cc') {
+      saldo += 50;
+      int bonus = 50;
+      print(
+        '\n Parabéns você acaba de criar sua conta e ganhou R\$$bonus para começar a usar',
+      );
     } else {
-      this.saldo += 150;
+      saldo += 150;
+      int bonus2 = 150;
+      print(
+        '\n Parabéns você acaba de criar sua conta e ganhou R\$$bonus2 para começar a usar',
+      );
     }
+    int? caso;
+    do {
+      stdout.write(
+        'O que deseja fazer agora: \n1- Depositar \n2-Sacar \n3-Pagar mensalidade \n4-Consultar minha conta \n5-FecharConta',
+      );
+      String? entrada3 = stdin.readLineSync();
+      caso = int.tryParse(entrada3 ?? "");
+      if (caso == null ||
+          (caso != 1 && caso != 2 && caso != 3 && caso != 4 && caso != 5)) {
+        print('\n\nERRO, por favor digite um numero entre 1 e 5');
+        continue;
+      }
+    } while (caso != 1 && caso != 2 && caso != 3 && caso != 4 && caso != 5);
   }
+  
 
   void fecharConta() {
     if (this.saldo == 0) {
@@ -129,7 +179,7 @@ class Conta {
 
   void meusdados() {
     print(
-      '-- Dados da Conta: -- \nNome: $nome \nTipo de Conta: $tipoConta \nSaldo: $saldo',
+      '-- Dados da Conta: -- \nNome: $nome \nTipo de Conta: $tipoConta \nSaldo: $saldo \nConta esta ativa? ${this.status ? "Sim" : "Não"}',
     );
   }
 }
