cf9e13bfb6b51c233cb39112bbe35452e0bb8525
dunoer <edjrdu12@gmail.com>
2025-09-26 16:25:55 -0300

    upadet

--- ex03/controle.dart
+++ ex03/controle.dart
@@ -1,4 +1,7 @@
-class ControleRemoto implements Controlador{
+import 'interfarce.dart';
+import 'dart:io';
+
+class ControleRemoto implements Controlador {
   //atribuitos
   int _volume = 0;
   bool _ligando = true;
@@ -34,5 +37,74 @@ class ControleRemoto implements Controlador{
     this._tocando = t;
   }
 
-  
+  @override
+  void abrirMenu() {
+    stdout.write(
+      '--- MENU --- \n\n\nEsta ligado: ${this.getLigando() ? 'sim' : 'não'} \nEsta tocando? ${this.getTocando() ? 'sim' : 'não'} \nVolume: ${this.getVolume()} ',
+    );
+    for (int i = 0; i < this.getVolume(); i += 10) {
+      stdout.write('|');
+    }
+  }
+
+  @override
+  void desligar() {
+    this.setLigando(false);
+  }
+
+  @override
+  void desligarMudo() {
+    if (this.getLigando() && this.getVolume() == 0) {
+      this.setVolume(50);
+    }
+  }
+
+  @override
+  void fecharMenu() {
+    print('\n\n----- Fechando Menu ----');
+  }
+
+  @override
+  void ligar() {
+    this.setLigando(true);
+  }
+
+  @override
+  void ligarMudo() {
+    if (this.getLigando() && this.getVolume() > 0) {
+      this.setVolume(0);
+    }
+  }
+
+  @override
+  void maisVolume() {
+    if (this.getLigando()) {
+      this.setVolume(this.getVolume() + 5);
+    } else {
+      print('Impossivel aumentar volume');
+    }
+  }
+
+  @override
+  void menosVolume() {
+    if (this.getLigando() && this.getVolume() > 0) {
+      this.setVolume(this.getVolume() - 5);
+    } else {
+      print('Impossivel diminuir volume');
+    }
+  }
+
+  @override
+  void pause() {
+    if (this.getLigando() && this.getTocando()) {
+      this.setTocando(false);
+    }
+  }
+
+  @override
+  void play() {
+    if (this.getLigando() && !(this.getTocando())) {
+      this.setTocando(true);
+    }
+  }
 }
--- ex03/main.dart
+++ ex03/main.dart
@@ -1,3 +1,10 @@
+import 'controle.dart';
+
 void main() {
-  
+  ControleRemoto c = new ControleRemoto();
+  c.ligar();
+  c.maisVolume();
+  c.play();
+  c.abrirMenu();
+  c.fecharMenu();
 }
